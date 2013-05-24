eeeb13c2780bb2932ad3ee87db1056f8c54c9c4f
unknown <xianlongice@gmail.com>
2013-05-24 17:18:18 +0800

    add to phpqr code

---
+++ phpQr/index.php
@@ -0,0 +1,87 @@
+<?php
+
+
+
+
+?>
+
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+<head> 
+	<title>QRHacker</title>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8">
+	<link rel="stylesheet" type="text/css" href="./css/main.css">
+<style type="text/css">
+
+
+
+</style>
+</head>
+<body>
+	<div id="head">
+		<div id="logo">
+			<label>
+				<strong>My &nbsp;phpQr &nbsp; project</strong> 
+				<br/>
+				  make &nbsp; by &nbsp;froyo  &amp; 2013-05-23</label>
+		</div>
+	</div>
+	<div id="content">
+		<div class="left">
+			<header>GENERATE</header>
+			<div class="box type">
+				<h4>Select input type</h4>
+				<div class="box-content type">
+					<ul>
+						<li class="text" data-input="text">
+							<a>
+								<span></span>text<small>new</small>
+							</a>
+						</li>
+						<li class="text" data-input="URL">
+							<a>
+								<span></span>URL<small>new</small>
+							</a>
+						</li>
+						<li class="text" data-input="Phone number">
+							<a>
+								<span></span>Phone number<small>new</small>
+							</a>
+						</li>
+						<li class="text" data-input="VCard connected detail">
+							<a>
+								<span></span>VCard connected detail<small>new</small>
+							</a>
+						</li>
+						<li class="text" data-input="Wifi Access ">
+							<a>
+								<span></span>Wifi Access<small>new</small>
+							</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div class="box input">
+				<h4> YOU INPUT<small>start with http://</small></h4>
+			</div>
+			<div class="box-content">
+				<textarea id="url" spellcheck="false" class="watching">http://</textarea>
+				<p class="info">Measure scans on QR codes and get detailed real-time statistics! <a href="http://www.qrhacker.com/business/" class="click-track" data-action="upgrade" data-src="left-no-subsc-small-url">Learn more</a></p>
+			</div>
+
+		</div>
+		<div class="center">
+			<div class="welcome">
+
+			</div>
+		</div>
+		<div class="right">
+
+
+		</div>
+
+	</div>
+
+
+
+</body>
+</html>
